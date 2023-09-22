ed60e1b76aee2396d57c02ccb11c3113338e1420
Semidime <semidime@protonmail.com>
2023-09-22 11:24:40 +0100

    feat: updates password hint messages.
    -initial password message will now display if password is too short;
    -password confirmation message now indicates if password is too short
    or if does not match initial password.

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const passwordInput = document.getElementById('password');
 const passwordConfInput = document.getElementById('password_conf');
-const passwordMismatchMsg = document.getElementById('password-mismatch');
+const passwordMsg = document.getElementById('password-message');
+const passwordConfMsg = document.getElementById('password-conf-message');
 
-
+passwordInput.addEventListener("input", (event) => {
+    if(passwordInput.value.length < 6) {
+        passwordInput.setCustomValidity("Password must be 6-12 characters");
+        passwordMsg.textContent = "  Password must be 6-12 characters";
+        passwordMsg.style.color = "darkgray";
+    } else {
+        passwordInput.setCustomValidity("");
+        passwordMsg.textContent = "";
+    }
+})
 
 passwordConfInput.addEventListener("input", (event) => {
-    if (passwordConfInput.value.length >= passwordInput.value.length && passwordInput.value !== passwordConfInput.value) {
+    if(passwordConfInput.value.length >= 1 && passwordConfInput.value.length < passwordInput.value.length) {
+        passwordConfInput.setCustomValidity("Passwords must be same length.");
+        passwordConfMsg.textContent = "  Passwords must be same length.";
+        passwordConfMsg.style.color = "darkgray";
+    } else if (passwordConfInput.value.length >= passwordInput.value.length && passwordInput.value !== passwordConfInput.value) {
         passwordConfInput.setCustomValidity("Passwords do not match.");
-        passwordMismatchMsg.textContent = "  Passwords do not match.";
+        passwordConfMsg.textContent = "  Passwords do not match.";
+        passwordConfMsg.style.color = "red";
     } else {
         passwordConfInput.setCustomValidity("");
-        passwordMismatchMsg.textContent = "";
+        passwordConfMsg.textContent = "";
     }
 })
